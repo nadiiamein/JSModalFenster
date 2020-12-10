e1aa7d3234126adf0108d28f0ef61cc63c46ee7e
Nadiia <npodufala15@gmail.com>
2020-12-10 13:44:45 +0200

    added console.log

--- index.js
+++ index.js
@@ -9,26 +9,63 @@ const fruits = [
         id: 3, title: 'Kuchen', price: 40, img:'https://e1.edimdoma.ru/data/recipes/0014/1902/141902-ed4_wide.jpg?1603287386'
     }
 ]
+
+const toHTML = fruit => `
+<div class="col">
+        <div class="card">
+            <img class="card-img-top" style="height: 300px" src="${fruit.img}" alt="${fruit.title}">
+            <div class="card-body">
+              <h5 class="card-title">${fruit.title}</h5>
+              <a href="#" class="btn btn-primary" data-btn="price" data-id="${fruit.id}">Mehr Erfahren</a>
+              <a href="#" class="btn btn-danger">Löschen</a>
+
+            </div>
+          </div>
+       </div>
+`
+
+function render() {
+    const html = fruits.map(toHTML).join('');
+    document.querySelector('#fruits').innerHTML =  html;
+}
+render();
+
 //title:string
 //closable:boolean
 //content: string
 //width: string ('400')
-const modal = $.modal({
-    title: 'Nadiia Modal',
+const priceModal = $.modal({
+
+    title: 'Priece für position',
     closable: true,
-    content:
-    `<h4>Modal is working</h4>
-    <h4>Lorem4  </h4>`,
     width: '400px',
     footerButtons: [
-        {text: 'Ok', type: 'primary', handler() {
-            console.log('Primary btn clicked');
-            modal.close()
+        {text: 'Schliessen', type: 'primary', handler() {
+            priceModal.close()
         }},
-        {text: 'Cancel', type: 'danger', handler() {
-            console.log('Danger btn clicked');
-            modal.close()
-        }}
-    ]
+           ]
+});
+const priceModal = $.modal({
+
+    title: 'Priece für position',
+    closable: true,
+    width: '400px',
+    footerButtons: [
+        {text: 'Schliessen', type: 'primary', handler() {
+            priceModal.close()
+        }},
+           ]
 });
 
+document.addEventListener('click', event => {
+    event.preventDefault()
+    const btnType = event.target.dataset.btn;
+    const id = +event.target.dataset.id;
+    if (btnType === 'price') {
+        const fruit = fruits.find(f => f.id === id);
+        priceModal.SetContent(`<p> Preice fur ${fruit.title}: <strong> ${fruit.price}euro</strong></p>`);
+
+      priceModal.open();
+
+    }
+})
